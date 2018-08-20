9809a9c67448bec8cb593d36441100e14b4ee67a
Konstantins Lesovojs <k.lesovoj@gmail.com>
2018-08-20 21:59:32 +0300

    added rudimentary navigation stub

--- app/src/main/java/me/lesovoy/metronome/MainActivity.kt
+++ app/src/main/java/me/lesovoy/metronome/MainActivity.kt
@@ -1,12 +1,39 @@
 package me.lesovoy.metronome
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.app_toolbar))
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.toolbar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.settings -> {
+//            Launch settings activity
+                Toast.makeText(this, "Settigns", Toast.LENGTH_SHORT).show()
+            }
+            R.id.about -> {
+//            Launch about activity
+                Toast.makeText(this, "About", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
